3ae0c901f802754ec74741307db19882c4538edc
mrkylebrown <57872567+mrkylebrown@users.noreply.github.com>
2020-01-07 21:56:11 -0700

    yup

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const navSlide = () => {
 	//Animate Links
 	navLinks.forEach((link,index)=>{ 
 		link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 2}s`;
-		console.log(index);
 	})
 }
 
