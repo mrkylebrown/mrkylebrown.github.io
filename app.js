8a92151b501eebdfe93db8a97fe122467371bc6d
mrkylebrown <57872567+mrkylebrown@users.noreply.github.com>
2020-02-11 15:54:54 -0700

    Kinda figued out Nav in JS

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ const navSlide = () => {
 	burger.addEventListener('click', ()=>{
 		navLinks.forEach((link,index)=>{ 
 		link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-		})
-	})
+		});
+	});
 	
-}
+};
 
 navSlide();
 
