211d7195553dc63c7ae366e0b3718900e7d8f4ae
mrkylebrown <57872567+mrkylebrown@users.noreply.github.com>
2020-02-11 18:05:15 -0700

    js nav 2

---
+++ nav.js
@@ -0,0 +1,199 @@
+// JavaScript Document
+function nav(){
+
+document.write( '<nav id=\"navigationBar\">\n' );
+
+document.write( '  <ul class=\"main\">\n' );
+	
+document.write( '    <li class=\"grades\"><a href=\"file:///C:/Users/brown/OneDrive/Desktop/GitHub/Unnamed%20Site%202/index.html#\">Home</a>\n' );
+	
+document.write( '	  <!-------------------------- Math 9 Courses---------------------------->\n' );
+	
+document.write( '    <li class=\"grades\"><a href=\"#\">Math 9</a>\n' );
+document.write( '      <ul class=\"sub\">\n' );
+document.write( '          <li><a href=\"Calendar.html#\" target=\"_blank\">Calendar</a></li>\n' );
+document.write( '          <li><a href=\"#\">Course Info</a></li>\n' );
+document.write( '          <li><a href=\"#\"><strong>Unit 1</strong><br><small>Square Roots and Surface Area </small></a></li>\n' );
+document.write( '		  <li><a href=\"#\"><strong>Unit 2</strong> <br><small>Powers and Exponents</small></a></li>\n' );
+document.write( '		  <li><a href=\"#\"><strong>Unit 3</strong> <br><small>Rational Numbers</small></a></li>\n' );
+document.write( '		  <li><a href=\"#\"><strong>Unit 4</strong> <br><small>Linear Relations</small></a></li>\n' );
+document.write( '		  <li><a href=\"#\"><strong>Unit 5</strong> <br><small>Polynomials</small></a></li>\n' );
+document.write( '		  <li><a href=\"#\"><strong>Unit 6</strong> <br><small>Linear Equations and Inequalities</small></a></li>\n' );
+document.write( '		  <li><a href=\"#\"><strong>Unit 7</strong> <br><small>Similarity and Transformations</small></a></li>\n' );
+document.write( '		  <li><a href=\"#\"><strong>Unit 8</strong> <br><small>Circle Geometry</small></a></li>\n' );
+document.write( '		  <li><a href=\"#\"><strong>Unit 9</strong> <br><small>Probability and Statistics</small></a></li>\n' );
+document.write( '      </ul>\n' );
+document.write( '    </li>\n' );
+
+document.write( '	 <!-------------------------- Math 10 Courses---------------------------->\n' );
+
+document.write( '    <li class=\"grades\"><a href=\"#\">Math 10 Courses</a>\n' );
+document.write( '      <ul class=\"sub\">\n' );
+document.write( '        <li><a href=\"#\">Math 10C<span class=\"right\"></span></a>\n' );
+document.write( '          	<ul class=\"sub sub2\">\n' );
+document.write( '				  <li><a href=\"#\">Calendar</a></li>\n' );
+document.write( '				  <li><a href=\"#\">Course Info</a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 1:</strong><br> <small>Roots & Powers</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 2:</strong><br> <small>Factors & Products</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 3:</strong><br> <small>Relations & Functions</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 4:</strong><br> <small>Linear Functions</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 5:</strong><br> <small>Systems of Linear Equations</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 6:</strong><br> <small>Measurement</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 7:</strong><br> <small>Trigonometry</small></a></li>\n' );
+document.write( '          	</ul>\n' );
+document.write( '        </li>\n' );
+
+document.write( '        <li><a href=\"#\">Math 10-3<span class=\"right\"></span></a>\n' );
+document.write( '			<ul class=\"sub sub2\">\n' );
+document.write( '				<li><a href=\"#\">Calendar</a></li>\n' );
+document.write( '				<li><a href=\"#\">Course Info</a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 1:</strong><br><small> Unit Pricing & Currency Exchange</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 2:</strong><br><small> Earning an Income</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 3:</strong><br><small> Length, Area & Volume</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 4:</strong><br><small> Mass, Temperature & Volume</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 5:</strong><br><small> Angles and Parallel Lines</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 6:</strong><br><small> Similar Figures</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 7:</strong><br><small> Trigonometry</small></a></li>\n' );
+document.write( '			</ul>\n' );
+document.write( '		</li>\n' );
+document.write( '      </ul>\n' );
+document.write( '    </li>\n' );
+
+document.write( '	  <!-------------------------- Math 20 Courses---------------------------->\n' );
+
+document.write( '    <li class=\"grades\"><a href=\"#\">Math 20 Courses</a>\n' );
+document.write( '	  	<ul class=\"sub\">\n' );
+document.write( '			<li><a href=\"#\">Math 20-1</a></li>\n' );
+document.write( '			<li><a href=\"#\">Math 20-2<span class=\"right\"></span></a>\n' );
+document.write( '				<ul class=\"sub sub2\">\n' );
+document.write( '					<li><a href=\"#\">Calendar</a></li>\n' );
+document.write( '				  <li><a href=\"#\">Course Info</a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 1:</strong><br> <small>Radicals</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 2:</strong><br> <small>Relations & Functions</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 3:</strong><br> <small>Merasurement</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 4:</strong><br> <small>Statistics</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 5:</strong><br> <small>Angles & Trig</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 6:</strong><br> <small>Course Review</small></a></li>\n' );
+document.write( '				</ul>\n' );
+document.write( '			</li>\n' );
+
+document.write( '			<li><a href=\"#\">Math 20-3<span class=\"right\"></span></a>\n' );
+document.write( '				<ul class=\"sub sub2\">\n' );
+document.write( '					<li><a href=\"#\">Calendar</a></li>\n' );
+document.write( '				  <li><a href=\"#\">Course Info</a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 1:</strong><br> <small>Slope & Rate of Change</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 2:</strong><br> <small>Graphical Representations</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 3:</strong><br> <small>Surface Area, Volume & Capacity</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 4:</strong><br> <small>Trigonometry</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 5:</strong><br> <small>Scale Representations</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 6:</strong><br> <small>Financial Services</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Ch. 6:</strong><br> <small>Budgets</small></a></li>\n' );
+document.write( '				</ul>\n' );
+document.write( '			</li>\n' );
+document.write( '	  	</ul>\n' );
+document.write( '	  </li>\n' );
+
+document.write( '	  <!-------------------------- Math 30 Courses---------------------------->\n' );
+
+document.write( '    <li class=\"grades\"><a href=\"#\">Math 30 Courses</a>\n' );
+document.write( '		<ul class=\"sub\">\n' );
+document.write( '			<li><a href=\"#\">Math 30-1</a></li>\n' );
+document.write( '			<li><a href=\"#\">Math 30-2<span class=\"right\"></span></a>\n' );
+document.write( '				<ul class=\"sub sub2\">\n' );
+document.write( '					<li><a href=\"#\">Calendar</a></li>\n' );
+document.write( '				  <li><a href=\"#\">Course Info</a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 1:</strong><br> <small>Radicals</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 2:</strong><br> <small>Relations & Functions</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 3:</strong><br> <small>Merasurement</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 4:</strong><br> <small>Statistics</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 5:</strong><br> <small>Angles & Trig</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Unit 6:</strong><br> <small>Course Review</small></a></li>\n' );
+document.write( '				</ul>\n' );
+document.write( '			</li>\n' );
+document.write( '			<li><a href=\"#\">Math 30-3</a></li>\n' );
+document.write( '			<li><a href=\"#\">Calculus</a></li>\n' );
+document.write( '	  	</ul>  \n' );
+document.write( '	</li>\n' );
+	
+document.write( '	  <!-------------------------- Physics Courses---------------------------->\n' );
+	
+document.write( '    <li class=\"grades\"><a href=\"#\">Physics Courses</a>\n' );
+document.write( '	  <ul class=\"sub\">\n' );
+document.write( '	  	<li><a href=\"#\">Physics 20</a></li>\n' );
+document.write( '		<li><a href=\"#\">Physics 30<span class=\"right\"></span></a>\n' );
+document.write( '				<ul class=\"sub sub2\">\n' );
+document.write( '					<li><a href=\"#\">Calendar</a></li>\n' );
+document.write( '				  <li><a href=\"#\">Course Info</a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Lessons 1-4:</strong><br> <small>Momentum, Impulse & Graphing Techniques</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Lessons 5-12:</strong><br> <small>Light, Geometric Optics, Mirrors and Lenses, & Diffraction Gratings</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Lessons 13-18:</strong><br> <small>Electrostatics and Electricity</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Lesson 19-24:</strong><br> <small>Magnetism</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Lesson 25-34:</strong><br> <small>Nature of the Atom, Particle Physics, & Quantum Mechanics</small></a></li>\n' );
+document.write( '				<li><a href=\"#\"><strong>Lesson 35-38:</strong><br> <small>Nuclear Physics</small></a></li>\n' );
+document.write( '				</ul>\n' );
+document.write( '			</li>\n' );
+document.write( '	  </ul>\n' );
+document.write( '	</li>\n' );
+
+document.write( '  </ul>\n' );
+document.write( '	<div class=\"allCoursesContainer\">\n' );
+document.write( '		<li class=\"allCourses\">All Courses</li>\n' );
+document.write( '		<div class=\"burger\">\n' );
+document.write( '				<div class=\"line1\"></div>\n' );
+document.write( '				<div class=\"line2\"></div>\n' );
+document.write( '				<div class=\"line3\"></div>\n' );
+document.write( '		</div>\n' );
+document.write( '	</div>\n' );
+document.write( '</nav>\n' );
+		
+			$(function(){
+        $(".main li").hover(
+            function(){
+                $('>ul.sub:not(:animated)', this).slideDown(500);
+            },
+            function(){
+                $('>ul.sub',this).slideUp(300);
+            }
+        );
+    });
+	// JavaScript Document
+const navSlide = () => {
+	var count = 0;
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.main');
+	const navLinks = document.querySelectorAll('.grades');
+	
+	// Click Burger toggles nav from hidden to on screen on mobil view
+	burger.addEventListener('click', ()=>{
+	nav.classList.toggle('nav-active');
+	nav.classList.remove('nav-active2');
+	nav.classList.remove('nav-active3');
+	count = 0;
+	});
+	
+	// Click the nav in mobile view slides left
+	nav.addEventListener('click', ()=>{
+		if (count == 0) {
+			nav.classList.toggle('nav-active2');
+		} else if (count == 1) {
+			nav.classList.toggle('nav-active3');
+		} 
+		
+	count += 1;
+	
+	});
+	
+
+	//Animate Links
+	burger.addEventListener('click', ()=>{
+		navLinks.forEach((link,index)=>{ 
+		link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+		});
+	});
+	
+};
+
+navSlide();
+
+}
+
